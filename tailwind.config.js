bf6cd9da6837741ba2b391f91c23eeb5cdc40845
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-06 02:12:51 -1200

    Configure screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "320px",
+      sm: "576px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px"
+    },
     extend: {
       colors: {
         "tomato": "#ff6347",
