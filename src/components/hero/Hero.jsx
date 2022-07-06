7afee419528a5557d3fc15ef4834f4d60d05a1b8
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-06 00:26:52 -1200

    Update hero.jsx profile image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Particle from "../particles/Particle";
+import Profile from "../../assets/images/profile.png";
 import "./hero.css";
 
 const Hero = () => {
   return (
     <div className="hero">
       <Particle />
-      <div className="hero-main w-full container mx-auto">
+      <div className="hero-main w-full container mx-auto h-4/5">
         <nav className="hero-navbar flex justify-between items-center w-full pt-4">
-          <h1 className="hero-navbar__brand text-5xl font-bold">
+          <h1 className="hero-navbar__brand text-6xl font-bold">
             Qream<span className="text-tomato">ville</span>
           </h1>
           <div className="hero-navbar__resume text-2xl">
@@ -20,8 +21,22 @@ const Hero = () => {
             </a>
           </div>
         </nav>
-        <div className="hero-profile">
-          <div className="hero-profile__image"></div>
+        <div className="hero-profile h-full">
+          <div className="hero-profile__image w-72">
+            <img
+              src={Profile}
+              alt="Balikis"
+              className="w-full h-auto overflow-hidden"
+            />
+          </div>
+          <h2 className="hero-profile__name text-6xl text-tomato mb-2">
+            Balikis Oyeleye
+          </h2>
+          <p className="text-2xl">I'm a Front-End Developer</p>
+          <div></div>
+          <button>
+            <a href="#projects">See My Works</a>
+          </button>
         </div>
       </div>
     </div>
