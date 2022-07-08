55afd5c290e8dfcc26d8cde4f8782a5550782660
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-07 21:15:40 -1200

    Create skills components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Hero, About } from "./components"
+import Skill from "./components/skills/Skill";
 
 function App() {
   return <div className="App">
     <Hero />
     <About />
+    <Skill />
   </div>;
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Hero } from "./hero/Hero"
 export { default as About } from "./about/About"
+export { default as Skills } from "./skills/Skill"
