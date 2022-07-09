7d862a4ff1b00104532f163203c9d57964f50153
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-09 02:50:17 -1200

    Update footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer bg-bg-blue border-t border-t-[#444]">
+      <div className="container mx-auto text-center py-4">
+        <p className="text-2xl">&copy; 2022 - Made with 💖 by Qreamville</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
